e2efa1f0281b129b5837303cc30580c11ebda53f
eduardosalustiano <saluservicogerais@gmail.com>
2023-07-03 19:35:24 -0300

    app

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app= express();
 
 app.get("/",async (rreq,res)=>{
- res.send("pagina inicial");
+ res.send("pagina");
 
 });
 app.listen(8080,);
  console.log("servidor iniciado na porta 8080: http://localhost:8080");
+ 
